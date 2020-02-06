d74f383d26f26b55590fe6f44722daba328b6e91
AjithRithik <43243561+AjithRithik@users.noreply.github.com>
2020-02-06 14:27:24 +0530

    Open graph added

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,12 @@ export default () => (
     <NextSeo
       title="First Page"
       description="this the first page"
+      openGraph={{
+        url: 'https://www.url.ie/a',
+        title: 'First page open',
+        description: 'First page open',
+        site_name: 'SiteName',
+      }}
     />
     <h1>First Page</h1>
     <p>
--- pages/second.tsx
+++ pages/second.tsx
@@ -8,6 +8,12 @@ export default () => (
     <NextSeo
       title="Second Page"
       description="this the Second page"
+      openGraph={{
+        url: 'https://www.url.ie/a',
+        title: 'second page open',
+        description: 'second page open',
+        site_name: 'SiteName',
+      }}
     />
     <h1>SecondPage Added to Page</h1>
     <Link href="/">
